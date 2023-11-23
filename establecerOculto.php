3863e798d1b22990fd3953cccc194b7a07d63992
Yanni Castle <yannicastle5@gmail.com>
2023-11-23 02:57:35 -0600

    establecerOculto

---
+++ establecerOculto.php
@@ -0,0 +1,4 @@
+<?php
+$ocultoSoltero=$_POST['ocultoSoltero'];
+echo ("El valor del campo oculto es: '$ocultoSoltero'<br />");
+?>
---
+++ formularioSimple.php
@@ -0,0 +1,10 @@
+<?php
+$nombre=$_POST['nombre'];
+$soltero=$_POST['soltero'];
+$edad=$_POST['edad'];
+$datoOculto=$_POST['datoOculto'];
+echo ("El nombre tecleado es: '$nombre'<br />");
+echo ("El estado soltero es: '$soltero'<br />");
+echo ("El rango de edad es: '$edad'<br />");
+echo ("El campo oculto vale: '$datoOculto'<br />");
+?>
