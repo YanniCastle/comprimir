b5de7992875785770884d5ced1be12c0c26b326a
Yanni Castle <yannicastle5@gmail.com>
2023-11-25 00:56:08 -0600

    enviarFichero page:137

--- enviarFichero.php
+++ enviarFichero.php
@@ -1,6 +1,11 @@
 <?php
-foreach($_FILES["fichero"] as $clave => $valor)
-{
-  echo("Propiedad: $clave ---- Valor: $valor<br />");
-}
-?>
+//if($_FILES["fichero"]["size"] <= 5120000)
+//{
+   foreach($_FILES["fichero"] as $clave => $valor)
+   {
+    echo("Propiedad: $clave ---- Valor: $valor<br />");
+   }
+//} 
+
+//else { echo ("El fichero es muy grande"); }
+?> 
