98fef41ec19f7b53d5ac54b762529524c52c1615
Yanni Castle <yannicastle5@gmail.com>
2023-11-25 22:35:06 -0600

    enviarFichero limitar

--- enviarFichero.php
+++ enviarFichero.php
@@ -1,11 +1,4 @@
 <?php
-//if($_FILES["fichero"]["size"] <= 5120000)
-//{
-   foreach($_FILES["fichero"] as $clave => $valor)
-   {
-    echo("Propiedad: $clave ---- Valor: $valor<br />");
-   }
-//} 
-
-//else { echo ("El fichero es muy grande"); }
-?> 
+foreach ($_FILES["fichero"] as $clave => $valor) {
+   echo ("Propiedad: $clave ---- Valor: $valor<br />");
+}
